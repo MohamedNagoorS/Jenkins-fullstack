50c6bd29946714833be5300080ac2f802af584b1
Mohamed Nagoor S <97577086+MohamedNagoorS@users.noreply.github.com>
2024-12-30 19:19:50 +0530

    Update Signup.jsx

--- frontend/register/src/Components/Signup.jsx
+++ frontend/register/src/Components/Signup.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 function Signup() {
     const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const navigate = useNavigate();
 
